fdb274e801352101fadcf497b38acd681e3c70ef
Martin Flucka <mfl@foryouandyourcustomers.com>
2023-07-25 09:57:26 +0200

    All sides assigned need attacker and defender admin

--- webapp/src/lib/game/guards.ts
+++ webapp/src/lib/game/guards.ts
@@ -7,7 +7,9 @@ export const sharedGuards = {
   gameFinished: () => false,
   gameStarted: () => false,
   allSidesAssigned: ({ context }: { context: SharedGameContext }) =>
-    context.users.find((user) => user.side === undefined) === undefined,
+    context.users.find((user) => user.side === undefined) === undefined &&
+    !!context.users.find((user) => user.side === 'defender' && user.isAdmin) &&
+    !!context.users.find((user) => user.side === 'attacker' && user.isAdmin),
   finishedAssigningSides: () => false,
   allRolesAssigned: () => false,
   finishedAssigningRoles: () => false,
