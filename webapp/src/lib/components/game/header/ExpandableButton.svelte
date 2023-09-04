5c6348293c425ff403df1549a8ab1e93ff50ac1a
Matias Meno <m@tias.me>
2023-09-04 17:34:55 +0200

    Prepared expandable buttons

---
+++ webapp/src/lib/components/game/header/ExpandableButton.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import CloseIcon from '~icons/lucide/x-circle'
+  let expanded = false
+</script>
+
+<div class="expandable" class:expanded>
+  <button class="unstyled icon" on:click={() => (expanded = true)}><slot name="icon" /></button>
+  <div class="content">
+    <slot name="content" />
+    <button class="unstyled icon" on:click={() => (expanded = false)}><CloseIcon /></button>
+  </div>
+</div>
+
+<style lang="postcss">
+  .expandable {
+    position: relative;
+    color: black;
+    .icon {
+      display: grid;
+      place-content: center;
+      border-radius: var(--radius-full);
+      background: white;
+      width: 1.5rem;
+      height: 1.5rem;
+      :global(svg) {
+        display: block;
+        width: 1rem;
+        height: 1rem;
+      }
+    }
+    &.expanded {
+      .content {
+        display: block;
+      }
+    }
+    .content {
+      --_padding: 0.25rem;
+      display: none;
+      position: absolute;
+      top: calc(0px - var(--_padding));
+      right: calc(0px - var(--_padding));
+      border-radius: var(--radius-full);
+      background: white;
+      padding: var(--_padding);
+      height: calc(1.5rem + var(--_padding) * 2);
+    }
+  }
+</style>
--- webapp/src/lib/components/game/header/Header.svelte
+++ webapp/src/lib/components/game/header/Header.svelte
@@ -1,6 +1,10 @@
 <script>
   import EmojiPicker from './EmojiPicker.svelte'
+  import ExpandableButton from './ExpandableButton.svelte'
   import RollbackButton from './RollbackButton.svelte'
+  import SettingsIcon from '~icons/lucide/settings'
+  import AudioIcon from '~icons/lucide/volume-2'
+  import HelpIcon from '~icons/lucide/shield-question'
 </script>
 
 <div class="header">
@@ -9,6 +13,18 @@
   <RollbackButton />
 
   <EmojiPicker />
+
+  <div class="actions">
+    <ExpandableButton>
+      <HelpIcon slot="icon" />
+    </ExpandableButton>
+    <ExpandableButton>
+      <AudioIcon slot="icon" />
+    </ExpandableButton>
+    <ExpandableButton>
+      <SettingsIcon slot="icon" />
+    </ExpandableButton>
+  </div>
 </div>
 
 <style lang="postcss">
@@ -17,7 +33,7 @@
     justify-content: space-between;
     align-items: center;
     background: linear-gradient(to bottom, #2b344800, #2b3448a9);
-    padding-inline: 3rem;
+    padding-inline: 3rem 1rem;
     height: 3rem;
     overflow: hidden;
   }
@@ -25,5 +41,9 @@
     font: var(--display-h2);
     font-size: var(--scale-4);
     text-transform: uppercase;
+  }
+  .actions {
+    display: flex;
+    gap: 1rem;
   }
 </style>
