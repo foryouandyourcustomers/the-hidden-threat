c5736e1e57cff64e455f04c5a50fb638fbf6f58e
Martin Flucka <mfl@foryouandyourcustomers.com>
2023-09-19 13:10:06 +0200

    Add role descriptions and abilities

--- webapp/src/lib/game/constants/characters.ts
+++ webapp/src/lib/game/constants/characters.ts
@@ -16,16 +16,7 @@ export const CHARACTERS = [
     id: 'order-manager',
     name: 'Auftragsmanagement',
     description:
-      'Die Auftragsmanagerin ist für die Koordination und Überwachung des Bestell- und Lieferprozesses des Unternehmens zuständig und kümmern sich auch um die Beauftragung vonGroßhandel und Lieferanten.',
-    ability:
-      'Darf den gesamten Spielzugablauf mit einer Verteidiger:in-Figur ihrer Wahl ausführen.',
-    side: 'defense',
-  },
-  {
-    id: 'it-specialist',
-    name: 'IT Fachkraft',
-    description:
-      'Die Auftragsmanagerin ist für die Koordination und Überwachung des Bestell- und Lieferprozesses des Unternehmens zuständig und kümmern sich auch um die Beauftragung vonGroßhandel und Lieferanten.',
+      'Die Auftragsmanager:innen sind für die Koordination und Überwachung des Bestell- und Lieferprozesses eines Unternehmens zuständig und kümmern sich auch um die Beauftragung von Großhandel und Lieferanten.',
     ability:
       'Darf den gesamten Spielzugablauf mit einer Verteidiger:in-Figur ihrer Wahl ausführen.',
     side: 'defense',
@@ -34,25 +25,34 @@ export const CHARACTERS = [
     id: 'dispatch-manager',
     name: 'Speditionsleitung',
     description:
-      'Die Auftragsmanagerin ist für die Koordination und Überwachung des Bestell- und Lieferprozesses des Unternehmens zuständig und kümmern sich auch um die Beauftragung vonGroßhandel und Lieferanten.',
+      'Die Leiter:innen in der Spedition müssen sicherstellen, das alle Produkte den hohen Qualitätsstandards entsprechen.',
     ability:
-      'Darf den gesamten Spielzugablauf mit einer Verteidiger:in-Figur ihrer Wahl ausführen.',
+      'Darf fragen, ob der/die Angreifer:in, eine Stufe angreift, wenn sie sich auf dieser Stufe oder auf einem angrenzenden Feld zu dazu befindet.',
     side: 'defense',
   },
   {
     id: 'quality-manager',
     name: 'Qualitätsmanagement',
     description:
-      'Die Auftragsmanagerin ist für die Koordination und Überwachung des Bestell- und Lieferprozesses des Unternehmens zuständig und kümmern sich auch um die Beauftragung vonGroßhandel und Lieferanten.',
+      'Die Qualitätsmanager:innen sind für die Sicherstellung der Qualität der Produkte verantwortlich. Sie stellen sicher, dass alle Standards eingehalten werden',
     ability:
-      'Darf den gesamten Spielzugablauf mit einer Verteidiger:in-Figur ihrer Wahl ausführen.',
+      'Darf fragen, ob der/die Angreifer:in sich auf seinem oder einem angrenzenden Feld, befindet.',
+    side: 'defense',
+  },
+  {
+    id: 'it-specialist',
+    name: 'IT Fachkraft',
+    description:
+      'Die IT-Spezialist:innen sind für die Sicherstellung von Informations-Systeme zuständig. Sie kümmern sich auch um den Schutz der Systeme gegen Cyberangriffe',
+    ability:
+      'Darf den Gegenstand „Digital Footprint“ als Joker für einen beliebigen Gegenstand einsetzen.',
     side: 'defense',
   },
   {
     id: 'frustrated',
-    name: 'Frustriert',
+    name: 'Frustration',
     description:
-      'Die Auftragsmanagerin ist für die Koordination und Überwachung des Bestell- und Lieferprozesses des Unternehmens zuständig und kümmern sich auch um die Beauftragung vonGroßhandel und Lieferanten.',
+      'Du wurdest bei der letzten Beförderungsrunde übergangen – schon wieder! Frustriert über diese andauernde Ungerechtigkeit ziehst du den Schlussstrich und verlässt deine Firma; aber nicht, ohne dich vorher durch einen Sabotageakt bei ihr zu „bedanken“ – als ihr ehemaliger Risikomanager weißt du schließlich über die Schwachstellen im System Bescheid',
     ability:
       'Darf den gesamten Spielzugablauf mit einer Verteidiger:in-Figur ihrer Wahl ausführen.',
     side: 'attack',
@@ -61,7 +61,7 @@ export const CHARACTERS = [
     id: 'disappointed',
     name: 'Enttäuscht',
     description:
-      'Die Auftragsmanagerin ist für die Koordination und Überwachung des Bestell- und Lieferprozesses des Unternehmens zuständig und kümmern sich auch um die Beauftragung vonGroßhandel und Lieferanten.',
+      'Als System-Administratorin hast du Zugriff auf die sensibelsten Daten. Durch einen Blick in die interne Kommunikation der Geschäftsführung erfährt deine Kollegin, dass länger geplant ist, eure Abteilung demnächst zu outsourcen. Zutiefst enttäuscht darüber, dass man euch vor vollendete Tatsachen stellen wird. Jetzt willst du ihnen einen Denkzettel verpassen',
     ability:
       'Darf den gesamten Spielzugablauf mit einer Verteidiger:in-Figur ihrer Wahl ausführen.',
     side: 'attack',
