6b1c169e26619d2aa06279bba61f6014fe8d3418
Martin Flucka <mfl@foryouandyourcustomers.com>
2023-10-23 14:01:57 +0200

    Add collected items result

--- webapp/src/lib/components/game/playing/reaction/result/ResultHasCollectedItems.svelte
+++ webapp/src/lib/components/game/playing/reaction/result/ResultHasCollectedItems.svelte
@@ -1,9 +1,47 @@
 <script lang="ts">
+  import { useSelector } from '$lib/@xstate/svelte/useSelector'
+  import { getGameContext } from '$lib/client/game-context'
   import GameDialog from '$lib/components/ui/GameDialog.svelte'
   import Paragraph from '$lib/components/ui/Paragraph.svelte'
+  import { BOARD_ITEMS } from '$lib/game/constants/board-items'
+  import { isAttackItemId, type AttackItemId } from '$lib/game/constants/items'
+  import { GameState } from '$lib/game/game-state'
+  import isEqual from 'lodash/isEqual'
+  import Item from '$lib/components/icons/Item.svelte'
+
+  const { machine } = getGameContext()
+
+  const collectedItems = useSelector(machine.service, ({ context }) => {
+    const gameState = GameState.fromContext(context)
+    const lastFinalizedActionEvent = gameState.finalizedActionEvents.at(-1)
+    if (
+      lastFinalizedActionEvent?.action !== 'ask-question' ||
+      lastFinalizedActionEvent.question !== 'has-collected-items'
+    ) {
+      return undefined
+    }
+
+    return BOARD_ITEMS.filter(
+      (item) =>
+        isAttackItemId(item.id) && isEqual(item.position, lastFinalizedActionEvent.position),
+    )
+      .map((item) => ({
+        id: item.id,
+        count: gameState.attackInventory[item.id as AttackItemId],
+      }))
+      .filter((item) => item.count)
+  })
 </script>
 
-<GameDialog title="Gesammelte Gegenstände" on:close>
-  <Paragraph>Der/die Angreifer:in hat folgende Gegenstände gesammelt:</Paragraph>
-  Noch nicht implementiert
-</GameDialog>
+{#if $collectedItems}
+  <GameDialog title="Gesammelte Gegenstände" on:close>
+    {#if $collectedItems.length > 0}
+      <Paragraph>Der/die Angreifer:in hat folgende Gegenstände gesammelt:</Paragraph>
+      {#each $collectedItems as item}
+        <Item itemId={item.id} />
+      {/each}
+    {:else}
+      <Paragraph>Der/die Angreifer:in hat keine der Gegenstände gesammelt.</Paragraph>
+    {/if}
+  </GameDialog>
+{/if}
