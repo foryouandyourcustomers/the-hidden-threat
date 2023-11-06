34e5bea025c3446d323ddf09f94c6e7d18aaeb64
Matias Meno <m@tias.me>
2023-11-06 17:21:04 +0100

    Add option to download game stats

--- webapp/src/lib/components/game/finished/Finished.svelte
+++ webapp/src/lib/components/game/finished/Finished.svelte
@@ -1,7 +1,34 @@
-<script>
+<script lang="ts">
+  import { useSelector } from '$lib/@xstate/svelte'
+  import { getGameContext } from '$lib/client/game-context'
+  import { getCurrentUser } from '$lib/client/game-machine/utils'
+  import { getSharedGameContext } from '$lib/client/game/utils'
   import Score from '$lib/components/game/playing/game-status/Score.svelte'
+  import type { SharedGameContext } from '$lib/game/types'
+  import format from 'date-fns/esm/format'
+
+  const { machine } = getGameContext()
+
+  const isAdmin = useSelector(machine.service, ({ context }) => getCurrentUser(context).isAdmin)
+
+  const sharedContext = useSelector(machine.service, ({ context }) => getSharedGameContext(context))
+
+  $: sharedContextData = $sharedContext
+    ? 'data:text/json;charset=utf-8,' +
+      encodeURIComponent(JSON.stringify($sharedContext, undefined, 2))
+    : null
+
+  const getFilename = (context: SharedGameContext) => {
+    const date = new Date(context.timestamp)
+
+    return `game-data-${format(date, 'yyyy-LL-dd')}.json`
+  }
 </script>
 
-Finished
-
 <Score />
+
+{#if $isAdmin && $sharedContext && sharedContextData}
+  {@const filename = getFilename($sharedContext)}
+  Download Data:
+  <a href={sharedContextData} download={filename}>{filename}</a>
+{/if}
