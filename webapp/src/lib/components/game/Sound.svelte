f46d7ac0da430781549676f42143593824805349
Matias Meno <m@tias.me>
2023-11-13 15:31:14 +0100

    Play sound when attacking stage

--- webapp/src/lib/components/game/Sound.svelte
+++ webapp/src/lib/components/game/Sound.svelte
@@ -3,25 +3,65 @@
   import { getGameContext } from '$lib/client/game-context'
   import { getCurrentUser } from '$lib/client/game-machine/utils'
   import { GameState } from '$lib/game/game-state'
-  import { play } from '$lib/sound'
+  import { play, type Sound } from '$lib/sound'
   import isEqual from 'lodash/isEqual'
+  import { onMount } from 'svelte'
+  import { get, type Readable } from 'svelte/store'
 
   const { machine } = getGameContext()
 
-  const positions = useSelector(machine.service, ({ context }) => {
+  const positions = useSelector(
+    machine.service,
+    ({ context }) => {
+      const gameState = GameState.fromContext(context)
+      if (getCurrentUser(context).side === 'attack') {
+        return gameState.playerPositions
+      } else {
+        const { attacker: _, ...playerPositions } = gameState.playerPositions
+        return playerPositions
+      }
+    },
+    isEqual,
+  )
+
+  const inventory = useSelector(machine.service, ({ context }) => {
     const gameState = GameState.fromContext(context)
+    let itemCounts: number[]
     if (getCurrentUser(context).side === 'attack') {
-      return gameState.playerPositions
+      itemCounts = Object.values(gameState.attackInventory)
     } else {
-      const { attacker: _, ...playerPositions } = gameState.playerPositions
-      return playerPositions
+      itemCounts = Object.values(gameState.defenseInventory)
     }
+
+    return itemCounts.reduce((acc, count) => acc + count, 0)
   })
 
-  let previousPlayerPositions = $positions
+  const attackedStages = useSelector(
+    machine.service,
+    ({ context }) => GameState.fromContext(context).attackedStages.length,
+  )
+  const defendedStages = useSelector(
+    machine.service,
+    ({ context }) => GameState.fromContext(context).defendedStages.length,
+  )
 
-  $: if (!isEqual(previousPlayerPositions, $positions)) {
-    play('move')
-    previousPlayerPositions = $positions
+  const soundOnChange = <T>(
+    store: Readable<T>,
+    sample: Sound,
+    triggerFunction = (prev: T, next: T) => !isEqual(prev, next),
+  ) => {
+    let previousValue = get(store)
+    return store.subscribe((value) => {
+      if (triggerFunction(previousValue, value)) {
+        play(sample)
+      }
+      previousValue = value
+    })
   }
+  onMount(() => soundOnChange(positions, 'move'))
+  onMount(() => soundOnChange(inventory, 'capture', (prev, next) => next > prev))
+  // TODO: replace sound
+  onMount(() => soundOnChange(attackedStages, 'select', (prev, next) => next > prev))
+  // TODO: replace sound
+  onMount(() => soundOnChange(defendedStages, 'select', (prev, next) => next > prev))
 </script>
