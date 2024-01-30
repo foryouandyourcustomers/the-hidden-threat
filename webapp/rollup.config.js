e89246e87a19451994fa0d713446ca7481958f1b
Matias Meno <m@tias.me>
2024-01-30 11:51:25 +0100

    Improve pm2 configuration

--- webapp/rollup.config.js
+++ webapp/rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
+import copy from 'rollup-plugin-copy'
 
 export default {
   input: './tools/prod-server.ts',
@@ -8,5 +9,15 @@ export default {
     dir: 'build',
     format: 'es',
   },
-  plugins: [typescript(), nodeResolve(), commonjs()],
+  plugins: [
+    typescript(),
+    nodeResolve(),
+    commonjs(),
+    copy({
+      targets: [
+        { src: './tools/build-package.json', dest: './build', rename: 'package.json' },
+        { src: './tools/pm2.config.cjs', dest: './build' },
+      ],
+    }),
+  ],
 }
